3290a2a22de69a9796af12bb6d4d86c7cfbe8974
mcastroal <marilyn.castroalonso@gmail.com>
2026-03-24 10:28:01 -0400

    feat: lloading page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,10 +1,14 @@
-import LoginForm from '@/components/forms/LoginForm';
+import { Suspense } from "react";
+import LoginForm from "@/components/forms/LoginForm";
+import Navbar from "@/components/layout/Navbar";
 
-
-export default function login() {
-    return (
-        <div className='flex flex-col w-screen min-h-screen items-center justify-start'>
-            <LoginForm />
-        </div>
-    )
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-dvh w-full flex-col bg-[var(--stone)]">
+      <Navbar />
+      <Suspense fallback={<p className="p-10 text-[var(--storm-blue)]">Loading…</p>}>
+        <LoginForm />
+      </Suspense>
+    </div>
+  );
 }
